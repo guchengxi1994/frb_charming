c7d0dd3dc921591a7de1ad508cffa3c00279bd44
壹零 <guchengxi1994@qq.com>
2024-11-13 17:26:33 +0800

    refactor

---
+++ chart_core/src/charts/mod.rs
@@ -0,0 +1,70 @@
+pub mod line_chart;
+
+use charming::{
+    element::{CoordinateSystem, ItemStyle, Label},
+    Chart,
+};
+
+#[derive(Debug)]
+pub struct BaseChart {
+    pub type_name: Option<String>,
+    pub id: Option<String>,
+    pub name: Option<String>,
+    pub coordinate_system: Option<String>,
+    pub label: Option<bool>,
+    pub item_style: Option<String>,
+    pub emphasis: Option<String>,
+}
+
+impl BaseChart {
+    pub fn new() -> BaseChart {
+        BaseChart {
+            type_name: None,
+            id: None,
+            name: None,
+            coordinate_system: None,
+            label: None,
+            item_style: None,
+            emphasis: None,
+        }
+    }
+
+    pub fn set_type(mut self, type_name: String) -> Self {
+        self.type_name = Some(type_name);
+        return self;
+    }
+
+    pub fn get_coordinate_system(&self) -> CoordinateSystem {
+        if let Some(coordinate_system) = self.coordinate_system.as_ref() {
+            match coordinate_system.as_str() {
+                "cartesian2d" => return CoordinateSystem::Cartesian2d,
+                "polar" => return CoordinateSystem::Polar,
+                "geo" => return CoordinateSystem::Geo,
+                "calendar" => return CoordinateSystem::Calendar,
+                "single" => return CoordinateSystem::Single,
+                "parallel" => return CoordinateSystem::Parallel,
+                _ => return CoordinateSystem::Cartesian2d,
+            }
+        }
+
+        return CoordinateSystem::Cartesian2d;
+    }
+
+    pub fn get_label(&self) -> Label {
+        if let Some(label) = self.label {
+            return Label::new().show(label);
+        }
+
+        return Label::new().show(false);
+    }
+
+    pub fn get_item_style(&self) -> ItemStyle {
+        return ItemStyle::new().border_radius(8);
+    }
+}
+
+pub trait BaseChartTrait {
+    fn default() -> Self;
+
+    fn compose(&self) -> Chart;
+}
---
+++ chart_core/src/lib.rs
@@ -0,0 +1,29 @@
+pub mod charts;
+
+use charming::ImageRenderer;
+
+use crate::charts::{line_chart::LineChartData, BaseChartTrait};
+
+pub fn new_line_chart(title: String, data: LineChartData) -> Option<Vec<u8>> {
+    let r = compose(title, data);
+    match r {
+        Ok(_r) => {
+            return Some(_r);
+        }
+        Err(_e) => {
+            println!("render Line chart Error: {:?}", _e);
+            return None;
+        }
+    }
+}
+
+fn compose(title: String, data: LineChartData) -> anyhow::Result<Vec<u8>> {
+    let mut chart = charts::line_chart::LineChart::default();
+    chart.data = data;
+    chart.title = title;
+    let c = chart.compose();
+    let mut renderer = ImageRenderer::new(1000, 800);
+    let r = renderer.render_format(charming::ImageFormat::Png, &c)?;
+
+    anyhow::Ok(r)
+}
